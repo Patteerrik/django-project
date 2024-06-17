01fab95cb2f2c0f62d2e7ad4be4993806fe9f499
Patteerrik . <patrik.akerbergz@gmail.com>
2024-06-17 09:14:55 +0000

    add deployment tasks

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-31+mic5z)9bceprj1g#1x@l2s&71t6mv^!&w2crh^23sd%(wwg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG=False
 
-ALLOWED_HOSTS = ["8000-patteerrik-djangoprojec-hsqs52ug0pi.ws-eu114.gitpod.io"]
+ALLOWED_HOSTS = ["8000-patteerrik-djangoprojec-hsqs52ug0pi.ws-eu114.gitpod.io"
+,'.herokuapp.com']
 
 
 # Application definition
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello_world',
+    'about',
 ]
 
 MIDDLEWARE = [
